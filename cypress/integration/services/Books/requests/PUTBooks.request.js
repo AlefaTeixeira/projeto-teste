ad46dda2ad82baaf8985dee8892b84745bd4606d
AlefaTeixeira <alefteixeira95@gmail.com>
2022-03-04 15:14:04 -0300

    Nova adicao de PUT

--- cypress/integration/services/Books/requests/PUTBooks.request.js
+++ cypress/integration/services/Books/requests/PUTBooks.request.js
@@ -1,11 +1,16 @@
 /// <reference types= "cypress" />
 
+const payloadChangeBook = require ('../payloads/change-book.js')
+
 function changeBook(idBook) {
     return cy.request({
         method: 'PUT',
         url: 'Books/${idBook}',
+        headers: {
+            'Context-type': 'application/json',
+        },
         failOnStatusCode: false,
-        body: payloadAddBook
+        body: payloadChangeBook
     })
 }
 
