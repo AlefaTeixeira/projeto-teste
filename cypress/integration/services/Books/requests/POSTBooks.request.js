2c35c30cfea1da8f126fba5a45557c16a2c31297
AlefaTeixeira <alefteixeira95@gmail.com>
2022-03-04 13:51:48 -0300

    Initial commit

---
+++ cypress/integration/services/Books/requests/POSTBooks.request.js
@@ -0,0 +1,14 @@
+/// <reference types= "cypress" />
+
+const payloadAddBook = require (' ../payloads/add-book.json')
+
+function addBooks() {
+    return cy.request({
+        method: 'POST',
+        url: 'Books',
+        failOnStatusCode: false,
+        body: payloadAddBook
+    })
+}
+
+export {allBooks};
---
+++ cypress/integration/services/Books/requests/PUTBooks.request.js
@@ -0,0 +1,12 @@
+/// <reference types= "cypress" />
+
+function changeBook(idBook) {
+    return cy.request({
+        method: 'PUT',
+        url: 'Books/${idBook}',
+        failOnStatusCode: false,
+        body: payloadAddBook
+    })
+}
+
+export {allBooks};
